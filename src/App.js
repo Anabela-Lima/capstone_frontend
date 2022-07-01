d8b01c012e4357b040481b361052ad8ed13e0faa
Haining Zhen <hainingzhen@outlook.com>
2022-07-01 10:39:48 +0100

    combined files

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Landing from './components/Landing';
 import LogIn from './components/LogIn';
+import MainPage from './components/MainPage/MainPage';
+import MainCarousel from './components/MainCarousel/MainCarousel';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/LogIn" element={<LogIn />} />
-          <Route path="/Home" element={<Home/>} />
-        </Routes>
-      </BrowserRouter>
+      {/* <MainCarousel /> */}
+      <MainPage />
     </>
   );
 }
 
 export default App;
+
+
+// <BrowserRouter>
+//         <Routes>
+//           <Route path="/LogIn" element={<LogIn />} />
+//           <Route path="/Home" element={<Home/>} />
+//         </Routes>
+//       </BrowserRouter>
--- src/components/MainCarousel/MainCarousel.js
+++ src/components/MainCarousel/MainCarousel.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './MainCarousel.css'
-import backgroundIslandImage from '../../assets/images/start_screen_background.png';
-import { ReactComponent as UserIcon } from '../../assets/images/user.svg';
-import { ReactComponent as PenIcon } from '../../assets/images/edit.svg';
-import { ReactComponent as MailIcon } from '../../assets/images/mail.svg';
-import { ReactComponent as LockIcon} from '../../assets/images/lock.svg';
+import backgroundIslandImage from '../../components/assets/images/start_screen_background.png';
+import { ReactComponent as UserIcon } from '../../components/assets/images/user.svg';
+import { ReactComponent as PenIcon } from '../../components/assets/images/edit.svg';
+import { ReactComponent as MailIcon } from '../../components/assets/images/mail.svg';
+import { ReactComponent as LockIcon} from '../../components/assets/images/lock.svg';
 
 const MainCarousel = () => {
 
