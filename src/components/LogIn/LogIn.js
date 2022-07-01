b245bebcb592dcf0db10510440b31455b58d481c
Scott Christie <s.christie496@gmail.com>
2022-07-01 13:00:22 +0100

    loginUpdates

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import './LogIn.css'
+import backgroundIslandImage from './../assets/images/start_screen_background.png';
+import { ReactComponent as UserIcon } from './../assets/images/user.svg';
+import { ReactComponent as PenIcon } from './../assets/images/edit.svg';
+import { ReactComponent as MailIcon } from './../assets/images/mail.svg';
+import { ReactComponent as LockIcon} from './../assets/images/lock.svg';
+import { Link } from 'react-router-dom';
+
+const LogIn = () => {
+
+  const [formInput, setFormInput] = React.useState({
+    username : "",
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: ""
+  })
+
+
+  const onRegistrationInputChange  = (e) => {
+    let currentUsername = formInput.username;
+    let currentFirstname = formInput.firstname;
+    let currentLastname = formInput.lastname;
+    let currentEmail = formInput.email;
+    let currentPassword = formInput.password;
+    switch(e.target.id) {
+        case "username-form":
+          currentUsername = e.target.value;
+          break;
+        case "firstname-form":
+          currentFirstname = e.target.value;
+          break;
+        case "lastname-form":
+          currentLastname = e.target.value;
+          break;
+        case "email-form":
+          currentEmail = e.target.value;
+          break;
+        case "password-form":
+          currentPassword = e.target.value;
+          break;
+        default:
+          break;
+    }
+    setFormInput({
+      username: currentUsername,
+      firstname: currentFirstname,
+      lastname: currentLastname,
+      email: currentEmail,
+      password: currentPassword
+    })
+  }
+
+
+  const PageIndicator = () => {
+    return (
+      <>
+        <div id="swipe-indicator-container">
+            <div id="swipe-indicators">
+              <div id="first-circle"></div>
+              <div id="second-circle"></div>
+              <div id="third-circle"></div>
+            </div>
+        </div>
+        <div id="page-name-container">
+          <span id="page-name-text">Sign in</span>
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div id="carousel-container">
+          <img src={backgroundIslandImage} alt="" id="background-img-log-in"/>
+              <div id="center-container">
+                <div id="register-form-container-log-in">
+                  <div id="log-in-title">Log In 
+                    <Link to="/">
+                    <button id="create-account-btn" value="submit"> or Create your New Account</button>
+                    </Link>
+                  </div>
+                  <div className="log-in-form-container">
+                    <UserIcon className="register-form-icons"/>
+                    <input 
+                      className="register-form"
+                      id="username-form"
+                      type="text"
+                      placeholder="Username"
+                      spellCheck="false"
+                      autoComplete='off'
+                      value={formInput.username}
+                      onChange={onRegistrationInputChange}
+                    ></input>
+                  </div>
+                  <div className="log-in-form-container">
+                    <LockIcon className="register-form-icons"/>
+                    <input 
+                      className="register-form" 
+                      id="password-form"
+                      type="password" 
+                      placeholder="Password"
+                      spellCheck="false"
+                      autoComplete='off'
+                      value={formInput.password}
+                      onChange={onRegistrationInputChange}
+                    ></input>
+                  </div>
+                </div>
+                <div id="pass-reg-container">
+                  <Link to="/ForgotPass">
+                  <button id="forgot-pass-btn">Forgot Password?</button>
+                  </Link>
+                  <div id="vertical-line"></div>
+                  <Link to="/">
+                  <button id="register-log-in-btn">Register</button>
+                  </Link>
+                </div>
+                <div id="sign-in-socials-container">
+                <Link to="/Facebook">
+                  <button id="forgot-pass-btn" value="submit">Forgot Password?</button>
+                  </Link>
+                  <div id="vertical-line"></div>
+                  <Link to="/">
+                  <button id="register-log-in-btn" value="submit">Register</button>
+                  </Link>
+                </div>
+              </div>
+              <div id="log-in-account-button-container">
+                <div id="log-in-account-button-background">
+                  <button id="log-in-account-button">
+                    <span id="log-in-account-button-text">Log in</span>
+                  </button>
+                </div>
+              </div>
+              <PageIndicator />
+          </div>
+    </>
+  )
+}
+
+export default LogIn;
