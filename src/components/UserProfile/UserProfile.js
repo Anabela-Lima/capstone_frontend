cb30eed7b73df910b484ed56a0f24714f9ce4645
Ana Bela Lima <a.blima@outlook.com>
2022-07-04 14:31:31 +0100

    mock-friends added

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -329,7 +329,7 @@ const UserProfile = ({goToTripScreenFromUserProfile, goToUserProfileFromTripScre
         },
         {
           day: 2,
-          name: "",
+          name: "Best Food",
           price: 950.00,
           category: "Food",
           imgUrl: "https://media-cdn.tripadvisor.com/media/photo-o/0d/53/b6/45/bistro.jpg",
@@ -389,35 +389,35 @@ const UserProfile = ({goToTripScreenFromUserProfile, goToUserProfileFromTripScre
       activities: [
         {
           day: 1,
-          name: "",
+          name: "tour of Delhi,Jaipur,Agra & Varanasi",
           price: 1000.00,
           category: "Physical",
-          imgUrl: "",
+          imgUrl: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/74/9c/e0.jpg",
           attendees: [1,2,3,4,5,6,10,21,18,15,13],
         },
         {
           day: 1,
-          name: "Ice Cave by Katla Volcano Super Jeep Tour from Vik",
+          name: "Day Excursion From Gangtok To Tsomgo Lake",
           price: 1800.00,
-          category: "Physical",
-          imgUrl: "",
+          category: "Travel",
+          imgUrl: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/ff/bb/ff.jpg",
           attendees: [1,2,3,4,6,8,10,12,16,19,20,21]
         },
        
         {
           day: 2,
-          name: "",
-          price: 4000.00,
-          category: "",
-          imgUrl: "",
+          name: "Alleppey Backwaters",
+          price: 500.00,
+          category: "Travel",
+          imgUrl: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/5e/59/d4/alleppey-backwater-tour.jpg?w=800&h=-1&s=1",
           attendees: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20]
         },
         {
           day: 2,
-          name: "Tanginn",
+          name: "Beas River",
           price: 900.00,
-          category: "Food",
-          imgUrl: "",
+          category: "Other",
+          imgUrl: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/46/76/ba/photo2jpg.jpg?w=1200&h=-1&s=1",
           attendees: [1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20]
         },
         {
@@ -425,24 +425,24 @@ const UserProfile = ({goToTripScreenFromUserProfile, goToUserProfileFromTripScre
           name: "Pakkhus Restaurant",
           price: 950.00,
           category: "Food",
-          imgUrl: "",
+          imgUrl: "https://youimg1.tripcdn.com/target/100h0j000000ao5638F2D_C_750_350_R5.jpg",
           attendees: [1,2,3,4,5,6,10,11,12,13,14,15,16,17,19]
         }
 
        ,{
           day: 2,
-          name: "",
+          name: "Jungle Swing & White Water Rafting Experience",
           price: 3000.00,
           category: "Entertainment",
-          imgUrl: "",
+          imgUrl: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/6f/70/d7.jpg",
           attendees: [1, 2,3,4,5,6,7,8,10,11,15,17,19,20]
         },
         {
           day: 3,
-          name: "",
-          price: 3800.00,
-          category: "Entertainment",
-          imgUrl: "",
+          name: "Nainital Lake",
+          price: 380.00,
+          category: "Other",
+          imgUrl: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/2e/b4/5d/nainital-lake.jpg?w=1200&h=-1&s=1",
           attendees: [1,2,3,6,10,11]
         }
 
@@ -470,43 +470,134 @@ const UserProfile = ({goToTripScreenFromUserProfile, goToUserProfileFromTripScre
     {
       firstname: 'Daniel',
       lastname: 'Davis',
-      imgUrl: "",
+      imgUrl: "https://compai.pub/v1/png/bc3324f77e9c016105ffe78b46a10776bba225ba67698ddd9f4b876892ab0ad6",
     },
     {
       firstname: "Jack",
       lastname: 'Feathers',
-      imgUrl: "",
+      imgUrl: "https://compai.pub/v1/png/13baed3193d76fb42d114d54e47850bde906c6557b6f9dc06651efe7f36a3b9c",
     },
     {
       firstname: "Summer",
       lastname: "Halts",
-      imgUrl: "",
+      imgUrl: "https://compai.pub/v1/png/e93816fb7ffdf3bffe2425cd45d1f57b18b4e76a12eae90b3f507c00cb73fea8",
     },
     {
       firstname: "Jess",
       lastname: "Bevers",
-      imgUrl: "",
+      imgUrl: "https://compai.pub/v1/png/43a437cc73d79df66187f8fac2ababb42b92b4b01c076143ba7ad938d5c30b96",
     },
     {
-      firstname: "Jonathan",
-      lastname: "Welsh",
-      imgUrl: ""
+      firstname: "Maisha",
+      lastname: "Preeti",
+      imgUrl: "https://compai.pub/v1/png/312387d8159fdc3690efaab5faf6419e468ae192c09fcd9564886f965046cbb2"
     },
     {
-      firstname: "Mary",
-      lastname: "Thomas",
-      imgUrl: ""
+      firstname: "Kayla",
+      lastname: "Moore",
+      imgUrl: "https://compai.pub/v1/png/b300caab6897731dfb914480ac6bd5acc20ca842a80c7aae123d3dc99d6257c6"
     },
     {
-      firstname: "Extra",
-      lastname: "Person",
-      imgUrl: ""
+      firstname: "Sam",
+      lastname: "Wilkinson",
+      imgUrl: "https://compai.pub/v1/png/4c1731569b9efbee7f54879e26818f3ddb252c165c8574e66ed1d072cc8b981d"
     },
     {
-      firstname: "Extra",
-      lastname: "Person",
-      imgUrl: ""
+      firstname: "James",
+      lastname: "Anderson",
+      imgUrl: "https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90JTIwZ3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
     }
+    ,
+    {
+      firstname: "Harry",
+      lastname: "Potterieye",
+      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvsoJ3qtqcRZ-Shgu84MKWjjzAv8pIeDQN4Oucmz0k_d5CLEMxWW0Rcni7mF2xLWSuOG0&usqp=CAU"
+    }
+    ,
+    {
+      firstname: "Jack",
+      lastname: "Swan",
+      imgUrl: "https://data.whicdn.com/images/346189618/original.jpg"
+    }
+    ,
+    {
+      firstname: "Leon",
+      lastname: "Max",
+      imgUrl: "https://i.pinimg.com/236x/1f/6a/b3/1f6ab372d9e89736cd35e12a775b43c0.jpg"
+    }
+    ,
+    {
+      firstname: "Noah",
+      lastname: "Jenkins",
+      imgUrl: "https://i.pinimg.com/originals/3e/93/ed/3e93edd2b130a05b81cc06a6c086e669.jpg"
+    }
+    ,
+    {
+      firstname: "Luke",
+      lastname: "Stillman",
+      imgUrl: "https://images.unsplash.com/photo-1593757147298-e064ed1419e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwZ3V5fGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+    }
+    ,
+    {
+      firstname: "Steve",
+      lastname: "Newman",
+      imgUrl: "https://www.picclickimg.com/d/l400/pict/402143831981_/Shirtless-Male-Handsome-Hunk-Beach-Jock-Facial-Hair.jpg"
+    }
+    ,
+    {
+      firstname: "Nathan",
+      lastname: "Williams",
+      imgUrl: "https://easyreadernews.com/wp-content/uploads/2019/03/Camp-surf-kid.jpg"
+    }
+    ,
+    {
+      firstname: "Mark",
+      lastname: "Holland",
+      imgUrl: "https://pbs.twimg.com/profile_images/3383710369/ca4291b8d16067615b935454a476f543_400x400.jpeg"
+    }
+    ,
+    {
+      firstname: "Erike",
+      lastname: "Sanderman",
+      imgUrl: "https://media.istockphoto.com/photos/cheering-to-the-good-music-picture-id1162968911?k=20&m=1162968911&s=612x612&w=0&h=tWmxIXYRDFUrk3pYMhWVEi7UuZHpPzw6tOCTRPlr4vo="
+    }
+    ,
+    {
+      firstname: "Joannah",
+      lastname: "Field",
+      imgUrl: "https://media.istockphoto.com/photos/happy-young-man-partying-on-a-music-festival-at-night-picture-id1131851632?k=20&m=1131851632&s=612x612&w=0&h=2SYZXM4YhKeArIz4baE9X-XrWnt29rXwbiy72lkz8-Q="
+    }
+    ,
+    {
+      firstname: "Maria",
+      lastname: "Beatriz",
+      imgUrl: "https://3.bp.blogspot.com/-lzE9TEaTNYY/U6xikcDmNoI/AAAAAAAAE7Q/rETzSYnz7J0/s1600/galore5.jpg"
+    }
+    ,
+    {
+      firstname: "Susana",
+      lastname: "Sousa",
+      imgUrl: "https://www.stockvault.net/data/2018/08/14/253777/preview16.jpg"
+    }
+    ,
+    {
+      firstname: "Layla",
+      lastname: "Herrords",
+      imgUrl: "https://d2rd7etdn93tqb.cloudfront.net/wp-content/uploads/2020/06/shutterstock-woman-with-palette-in-front-of-wave-paintings-061520.jpg"
+    }
+    ,
+    {
+      firstname: "Jennifer",
+      lastname: "Patterson",
+      imgUrl: "https://t4.ftcdn.net/jpg/00/48/12/29/360_F_48122956_hrYkTYxUooBAB1d5LcEEvcFzlAqvBN2i.jpg"
+    }
+    ,
+    {
+      firstname: "Tom",
+      lastname: "Spence",
+      imgUrl: "https://t4.ftcdn.net/jpg/02/02/32/23/360_F_202322352_pbZTNZw1G5NJJzpKpPoSHpAemonnjDkP.jpg"
+    }
+    
   ]
 
   
