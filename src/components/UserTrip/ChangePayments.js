7f6c480785a9ed7b288caad8bc29c634ea6c5217
Haining Zhen <hainingzhen@outlook.com>
2022-07-07 01:01:51 +0100

    split css

--- src/components/UserTrip/ChangePayments.js
+++ src/components/UserTrip/ChangePayments.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Table } from "react-bootstrap"
 
 
 const ChangePayments = ({assignment, reRender, reRenderAddUp}) => {  
@@ -33,21 +34,131 @@ const ChangePayments = ({assignment, reRender, reRenderAddUp}) => {
 
     return (
         <>
-        <h1 style={{color: "white"}}>
-            {assignment.applicationUser.firstname}
-            <form onSubmit={SubmitPaymentChange}>
-                <label>
-                    <input type="number" step="any" onChange={handlePaidChange} value={paid}/>
-                </label>
-                <label>
-                    <input type="number" step="any" onChange={handleShouldPayChange} value={shouldPay}/>
-                </label>
-            <input type="submit"/>
-            </form>
-            <button onClick={onRemove}>
-                Remove
-            </button>
-        </h1>
+            {/* <div style={{
+                    color: "white", 
+                    display: "flex", 
+                    flexDirection: "row", 
+                    alignItems: "center",
+                    justifyContent: "space-evenly"
+                }}
+            >
+                {assignment.applicationUser.firstname}
+                <form onSubmit={SubmitPaymentChange}>
+                    <label>
+                        <input 
+                            type="number" 
+                            step="any" 
+                            onChange={handlePaidChange} 
+                            value={paid}
+                        />
+                    </label>
+                    <label>
+                        <input 
+                            type="number" 
+                            step="any" 
+                            onChange={handleShouldPayChange} 
+                            value={shouldPay}
+                        />
+                    </label>
+                    <input type="submit"/>
+                </form>
+                <button onClick={onRemove}>
+                    Remove
+                </button>
+            </div> */}
+            {/* <Table id= "fieldsTable" striped bordered hover variant="light">
+                <thead>
+                    <tr>
+                        <th className="nameTh">Attendee</th>
+                        <th className="timeLeft">Paid</th>
+                        <th className="timeLeft">ShouldPay</th>
+                        <th className="">Submit</th>
+                        <th className="">Remove</th>
+                    </tr>
+                </thead> */}
+                    <tr>
+                        <td style={{
+                                fontSize: '40px', 
+                                textAlign: 'center',
+                                color: 'white'
+                            }}
+                        >
+                            {assignment.applicationUser.firstname}
+                        </td>
+
+                        <td>
+                            <label>
+                                <input 
+                                    type="number" 
+                                    step="any" 
+                                    onChange={handlePaidChange} 
+                                    value={paid}
+                                    style={{
+                                        height: '40px',
+                                        width: '80px',
+                                        paddingLeft: '10px',
+                                        marginLeft: '20px',
+                                        fontSize: '30px',
+                                        textAlign: 'center',
+                                        backgroundColor: 'transparent',
+                                        border: 'none',
+                                        color: 'white'
+                                    }}
+                                />
+                            </label>
+                        </td>
+
+                        <td>
+                            <label>
+                                <input 
+                                    type="number" 
+                                    step="any" 
+                                    onChange={handleShouldPayChange} 
+                                    value={shouldPay}
+                                    style= {{
+                                        height: '40px',
+                                        width: '100px',
+                                        marginLeft: '40px',
+                                        fontSize: '30px',
+                                        textAlign: 'center',
+                                        backgroundColor: 'transparent',
+                                        border: 'none',
+                                        color: 'white'
+                                    }}
+                                />
+                            </label>
+                        </td>
+
+                        <td>
+                            <input type="submit" style={
+                                    {
+                                        height: '70px', 
+                                        width: '140px',
+                                        marginLeft: '10px',
+                                        fontSize: '30px',
+                                        borderRadius: '20px',
+                                        border: 'none'
+                                    }
+                                }
+                            />
+                        </td>
+
+                        <td style={{paddingLeft: '10px'}}>
+                            <button 
+                                onClick={onRemove} 
+                                style={{
+                                    height: '70px', 
+                                    width: '140px',
+                                    fontSize: '30px',
+                                    borderRadius: '20px',
+                                    border: 'none'
+                                }}>
+                                Remove
+                            </button>
+                        </td>
+
+                    </tr>
+            {/* </Table> */}
         </>
     )
 };
--- src/components/UserTrip/Day.js
+++ src/components/UserTrip/Day.js
@@ -69,6 +69,7 @@ const Day = ({dayDetails, goToUserProfileFromTripScreen, dayNumber}) => {
                     <Activity 
                         reRender={reRenderActivites}
                         activity={activity}
+                        dayNumber={dayNumber}
                     />
                 </>
             )
@@ -100,6 +101,7 @@ const Day = ({dayDetails, goToUserProfileFromTripScreen, dayNumber}) => {
                     </animated.div>
                 :
                     ''
+
             })
         }
 
@@ -136,7 +138,8 @@ const Day = ({dayDetails, goToUserProfileFromTripScreen, dayNumber}) => {
                             <animated.div 
                                 style={style}
                             >
-                                <span style={{color: 'white', fontSize: '50px'}}>{activityBoolean.id}</span>
+                                <span 
+                                    style={{color: 'white', fontSize: '50px'}}>{activityBoolean.id}</span>
                             </animated.div> :
                             <animated.div 
                                 style={style} 
